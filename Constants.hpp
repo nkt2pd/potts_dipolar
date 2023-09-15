2f4979f3f925005d420c53c2bfd9ba3e6f3b0244
Quentin Simmons <nkt2pd@virginia.edu>
2023-09-15 12:17:31 -0400

    J = 0

--- Constants.hpp
+++ Constants.hpp
@@ -3,8 +3,8 @@
 
 #define N_nn1 6 //number of nearest neighbors
 #define q 6     //number of potts states
-#define Jnn -1  //Nearest-Neighbor ferromagnetic coupling
-#define Dp 0.14 //Long Range dipolar interaction
+#define Jnn 0  //Nearest-Neighbor ferromagnetic coupling
+#define Dp 1 //Long Range dipolar interaction
 #define PI 3.141592653589793238462643383279
 
 #endif
