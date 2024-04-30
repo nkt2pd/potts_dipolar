4a762c8a42d71e6636c9df33a92621fc8b32a342
nkt2pd <nkt2pd@virginia.edu>
2024-04-29 22:15:24 -0400

    config files updates

---
+++ plotpottscolor.plt
@@ -0,0 +1,31 @@
+#Blue = 1 + square
+#Red = -1 + circle
+
+#pt 7 is circles, pt 5 is squares
+
+
+
+unset logscale
+set xrange [-7.5:55.5]
+set yrange [-7.5:50]
+
+set size square
+
+myPt(col)    = column(col)==1 ? 5 : 5
+myColor(col) = column(col)==0 ? 0x1000ff : column(col)==1 ? 0xEFFF00 : column(col)==2 ? 0x03FF00 : column(col)==3 ? 0xFC00FF : column(col)==4 ? 0x00FEFF : 0xFF0100
+myShift(col) = column(col)>47  ? column(col) - 48 : column(col)
+unset key
+unset terminal
+unset tics
+unset title
+unset xlabel
+unset ylabel
+unset y2label
+set grid x,y
+
+unset multiplot
+
+set title "D = 0.025 after 2 KT"
+plot "uniformconfig.dat" using (myShift(1)):2:(myPt(4)):(myColor(3)) w p ps 1 pt var lc rgb var
+pause -1 "Hit return to continue"
+
--- printconfig.cpp
+++ printconfig.cpp
@@ -21,8 +21,8 @@ int main() {
     const int Ns = L*L;
 
     const double J = 1;
-    const double D = 0.15;
-    int h = 3;
+    const double D = 0.75;
+    int h = 1;
 
     Site* spin = new Site[Ns];
     Properties main_properties;
@@ -31,10 +31,16 @@ int main() {
 
     set_coordinates(spin, Ns, L);
     set_nn(spin, Ns, L);
-    init_dstripe(main_measurements, main_interactions, spin, Ns, L, h);
+    // init_stripe(main_measurements, main_interactions, spin, Ns, L, h);
+    init_uniform(main_measurements, main_interactions, spin, Ns, L);
 
-    std::string config_name = "ground_L" + std::to_string(L) + "_D" + std::to_string(D) + ".dat";
+    // std::string config_name = "ground_L" + std::to_string(L) + "_D" + std::to_string(D) + ".dat";
+    std::string config_name = "uniformconfig.dat";
     std::ofstream config(config_name);
+
+    for(int i = 0; i < Ns; i++) {
+        config << spin[i].x << ", " << spin[i].y << ", " << spin[i].potts << ", " << spin[i].Sz << std::endl;
+    }
 
     // for(int i = 0; i < L; i++) {
     //     if(spin[i*h].potts == 0) {
@@ -46,25 +52,25 @@ int main() {
     //         config << spin[h*i + j].x << ", " << spin[h*i + j].y << ", " << random_potts << ", " << spin[h*i + j].Sz << std::endl;
     //     }
     // }
-    int layer = 1;
-    int random_potts = 2*(int)(3*rand1());
+    // int layer = 1;
+    // int random_potts = 0;
 
-    for(int y = 0; y < L; y++) {
-        for(int x = 0; x < L; x++) {
-            config << spin[y*L + x].x << ", " << spin[y*L + x].y << ", " << random_potts << ", " << spin[y*L + x].Sz << std::endl;
-        }
-        if(layer%h == 0) {
-            if(rand1() < 0.5) {
-                random_potts += 1;
-            }else {
-                random_potts -= 1;
-            }
-            random_potts = mod(random_potts, q);
-            layer = 1;
-        } else{
-            layer++;
-        }
-    }
+    // for(int y = 0; y < L; y++) {
+    //     for(int x = 0; x < L; x++) {
+    //         config << spin[y*L + x].x << ", " << spin[y*L + x].y << ", " << random_potts << ", " << spin[y*L + x].Sz << std::endl;
+    //     }
+    //     if(layer%h == 0) {
+    //         if(rand1() < 0.5) {
+    //             random_potts += 1;
+    //         }else {
+    //             random_potts -= 1;
+    //         }
+    //         random_potts = mod(random_potts, q);
+    //         layer = 1;
+    //     } else{
+    //         layer++;
+    //     }
+    // }
 
     config.close();
 
