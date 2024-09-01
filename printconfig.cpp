020d7cca468bf900e670fd571608fe29c1c29f1a
nkt2pd <nkt2pd@virginia.edu>
2024-09-01 14:35:28 -0400

    some changes

--- printconfig.cpp
+++ printconfig.cpp
@@ -35,12 +35,12 @@ int main() {
     // init_uniform(main_measurements, main_interactions, spin, Ns, L);
 
     // std::string config_name = "ground_L" + std::to_string(L) + "_D" + std::to_string(D) + ".dat";
-    std::string config_name = "DJ0.750000_init_config.dat";
+    std::string config_name = "DJ0.750000_init_config2.dat";
     std::ofstream config(config_name);
 
-    // for(int i = 0; i < Ns; i++) {
-    //     config << spin[i].x << ", " << spin[i].y << ", " << spin[i].potts << ", " << spin[i].Sz << std::endl;
-    // }
+    for(int i = 0; i < Ns; i++) {
+        config << spin[i].x << ", " << spin[i].y << ", " << spin[i].potts << ", " << spin[i].Sz << std::endl;
+    }
 
     // for(int i = 0; i < L; i++) {
     //     if(spin[i*h].potts == 0) {
@@ -52,25 +52,25 @@ int main() {
     //         config << spin[h*i + j].x << ", " << spin[h*i + j].y << ", " << random_potts << ", " << spin[h*i + j].Sz << std::endl;
     //     }
     // }
-    int layer = 1;
-    int random_potts = 0;
+    // int layer = 1;
+    // int random_potts = 0;
 
-    for(int y = 0; y < L; y++) {
-        for(int x = 0; x < L; x++) {
-            config << spin[y*L + x].x << ", " << spin[y*L + x].y << ", " << random_potts << ", " << spin[y*L + x].Sz << std::endl;
-        }
-        if(layer%h == 0) {
-            if(rand1() < 0.5) {
-                random_potts += 1;
-            }else {
-                random_potts -= 1;
-            }
-            random_potts = mod(random_potts, q);
-            layer = 1;
-        } else{
-            layer++;
-        }
-    }
+    // for(int y = 0; y < L; y++) {
+    //     for(int x = 0; x < L; x++) {
+    //         config << spin[y*L + x].x << ", " << spin[y*L + x].y << ", " << random_potts << ", " << spin[y*L + x].Sz << std::endl;
+    //     }
+    //     if(layer%h == 0) {
+    //         if(rand1() < 0.5) {
+    //             random_potts += 1;
+    //         }else {
+    //             random_potts -= 1;
+    //         }
+    //         random_potts = mod(random_potts, q);
+    //         layer = 1;
+    //     } else{
+    //         layer++;
+    //     }
+    // }
 
     config.close();
 
