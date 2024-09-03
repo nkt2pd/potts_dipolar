856f0cb9dfbf5b16af7c696c895d6ca4c7178120
Quentin Simmons <nkt2pd@virginia.edu>
2024-09-03 10:18:25 -0400

    small fixes for D = 0.025 sim

--- Constants.hpp
+++ Constants.hpp
@@ -7,6 +7,6 @@
 #define Dp 1 //Long Range dipolar interaction
 #define PI 3.141592653589793238462643383279
 #define THERM_TIME 1800 //30 min
-#define SIM_TIME 28800 //8 hours
+#define SIM_TIME 14400  //4 hours
 
 #endif
